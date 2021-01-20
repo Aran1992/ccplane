68164eddf625ba0b81f64c438e7ac6a8cd580835
linzongda <linzongda@4399inc.com>
2021-01-20 13:01:30 +0800

    fixed git更新自动修改文件换行符

--- assets/Script/Utils.ts
+++ assets/Script/Utils.ts
@@ -1,9 +1,17 @@
 export default class Utils {
-    public static angle2radian(angle) {
+    public static angle2radian(angle: number): number {
         return angle / 180 * Math.PI;
     }
 
-    public static radian2angle(radian) {
+    public static radian2angle(radian: number): number {
         return radian / Math.PI * 180;
+    }
+
+    public static formatAngle(angle: number): number {
+        if (angle >= 0) {
+            return angle % 360;
+        } else {
+            return angle % 360 + 360;
+        }
     }
 }
